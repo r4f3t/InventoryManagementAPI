bcc6cd35ab1c6ffef233567c59509c1be1385622
r4f3t <parlakrafet01@gmail.com>
2020-01-18 17:35:55 +0300

    startup-dynamic-connection-added

---
+++ IM.Core/Domains/User.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace IM.Core.Domains
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+    }
+}
--- InventoryManagementApi/Startup.cs
+++ InventoryManagementApi/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IM.Core.Configurator;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 namespace InventoryManagementApi
 {
@@ -26,6 +28,13 @@ namespace InventoryManagementApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var monitor = services.BuildServiceProvider().GetService<IOptionsMonitor<CurrentSettings>>();
+            services.Configure<CurrentSettings>(Configuration.GetSection("CurrentSettings"));
+            if (monitor.CurrentValue.DatabaseSettings.CurrentDatabase == IM.Core.Enums.CurrentDatabase.MongoDB)
+            {
+                services.AddSingleton<IMongoClient>((a) => new MongoClient(monitor.CurrentValue.MongoDBSettings.ConnectionString));
+                //services.AddScoped<IRepository<IM_Databases>, MongoRepository<IM_Databases>>();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
